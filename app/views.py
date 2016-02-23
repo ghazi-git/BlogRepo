ad438c5303cd1b7dd46c071c4de430763d5fd31c
ghazi-git <abbassi@softcatalyst.com>
2016-02-23 20:05:13 +0100

    new updates with post detail pages

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 Definition of views.
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from django.http import HttpRequest
@@ -53,3 +53,7 @@ def about(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post_detail.html', {'post': post})
