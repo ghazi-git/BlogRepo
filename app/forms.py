501180f76438358b6783111b7cc4837b90d21c28
ghazi-git <abbassi@softcatalyst.com>
2016-02-24 11:34:32 +0100

    finalizing blog

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ Definition of forms.
 """
 
 from django import forms
+from .models import Post
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
@@ -16,3 +17,10 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text', )
